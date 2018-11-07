a7664124605fbfc42f8a7609cc655e26d1313626
Wameedh <wameedh.ali@hotmail.com>
2018-11-07 11:57:52 +0100

    Create new paths

--- client/src/components/Modules.jsx
+++ client/src/components/Modules.jsx
@@ -1,31 +1,91 @@
 import React, { Component} from 'react';
-import { getModules } from '../api/modules';
+import { getModules , createModule} from '../api/modules';
 
 class Modules extends Component {
-  state = {
-    modules: []
-  };
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      modules: []
+    };
+  }
 
-  componentDidMount() {
+   componentDidMount() {
     getModules().then((modules) => {
       this.setState({ modules: modules });
     });
   }
-
+ 
+  addNewModule = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+  addModule = e => {
+    e.preventDefault();
+    createModule(this.state.title).then(newPath => {
+      this.setState({
+        modules: this.state.modules.concat(newPath),
+        title: ""
+      });
+    });
+  };
+  
   render() {
     const { modules } = this.state;
-
-    if (modules.length > 0) {
-      return (
-        <ul>
-          {modules.map((module) => <li key={module._id}>{module.title}</li>)}
-        </ul>
-      )
-    } else {
-      return (
-        <p>There are no modules yet</p>
-      )
-    }
+  
+    return (
+      <div>
+        <div 
+          className="nav"
+        >
+          <h1>
+              DIGITAL EMPOWERMENT
+          </h1>
+          <a onClick={this.MangageUsers}>
+            <span>
+              Mangage users 
+            </span>
+          </a>
+          <a onClick={this.logOut}>
+            <span>
+              Log out 
+            </span>
+          </a>
+        </div>
+        <div 
+          className="continer"
+        >
+            <div 
+              className="Paths"
+            >
+              <h1>
+                Paths
+              </h1>
+              <input
+                className="search"
+                type='search'
+                placeholder='Search for paths...' 
+                onChange={this.searchItem}
+              />
+              <input
+                className="new-path"
+                type="text"
+                placeholder="Add new path"
+                onChange={this.addNewModule}
+                value={this.state.title}
+              />
+                <button 
+                  onClick={this.addModule}>
+                  Add path
+                </button>
+            </div>
+            <div class="continer_paths"> 
+                {modules.map((module) => <div className="items_paths" key={module._id}>{module.title}</div>)}
+            </div>
+          </div>   
+      </div>
+    );
   }
 }
 
