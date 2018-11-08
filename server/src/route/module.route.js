1c6790b1f97dfe71094ea2219b0752098d8a0d7d
Wameedh <38753468+WameedhAli@users.noreply.github.com>
2018-11-08 09:16:41 +0100

    Update module.route.js

--- server/src/route/module.route.js
+++ server/src/route/module.route.js
@@ -3,5 +3,4 @@ const modules = require('../controller/module.controller.js');
 module.exports = (app) => {
   app.get('/module', modules.findAll);
   app.post('/module', modules.create);
-  // app.delete('/module', modules.require);
 };
