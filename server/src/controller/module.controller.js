d47d66d8c6cf62040275d59d2cca60f04a56d9fd
Wameedh <38753468+WameedhAli@users.noreply.github.com>
2018-11-09 16:00:46 +0100

    Add checkbox code

--- server/src/controller/module.controller.js
+++ server/src/controller/module.controller.js
@@ -16,6 +16,30 @@ exports.create = (req, res) => {
     .save()
     .then((data) => { res.send(data); })
     .catch((err) => {
-      res.status(500).send({ message: err.message });
+      res.status(500).send({ 
+        message: err.message });
     });
+};
+
+exports.destroy = (req, res) => {
+  Module
+  .deleteOne({ _id: req.params.id })
+  .then((data) => { res.send(data); })
+  .catch((err) => {
+    res.status(500).send({
+      message: err.message
+    });
+  });
+};
+
+
+exports.require = (req, res) => {
+  Module
+  .updateOne({ completed: req.params.completed })
+  .then((data) => { res.send(data); })
+  .catch((err) => {
+    res.status(500).send({
+      message: err.message
+    });
+  });
 };
