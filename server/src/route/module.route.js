9a6da9c98a7417cce86637a5b39fada8fe7b5371
Wameedh <38753468+WameedhAli@users.noreply.github.com>
2018-11-09 16:01:17 +0100

    Add checkbox code

--- server/src/route/module.route.js
+++ server/src/route/module.route.js
@@ -3,4 +3,6 @@ const modules = require('../controller/module.controller.js');
 module.exports = (app) => {
   app.get('/module', modules.findAll);
   app.post('/module', modules.create);
+  app.delete('/module/:id', modules.destroy);
+  app.get('/module/:completed',modules.require);
 };
