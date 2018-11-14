fdb1168ac4eefefaf39ecb3c1de5e70629860c33
Wameedh <38753468+WameedhAli@users.noreply.github.com>
2018-11-14 21:13:43 +0100

    Add the WYSIWYG editor

--- client/src/components/Module.js
+++ client/src/components/Module.js
@@ -1,6 +1,9 @@
+
 import React from 'react';
 import{ Button ,Modal} from 'react-bootstrap'
 import { Component } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 class Module extends Component{
     constructor(props){
@@ -8,7 +11,7 @@ class Module extends Component{
     this.state={
         show:false,
         selectedModule:null,
-        title:props.module.tilte
+        title:props.module.tilte,
         }
     }
     handleDialoge=(module)=>{
@@ -16,10 +19,7 @@ class Module extends Component{
         this.setState({show:!this.state.show})
         
     }
-    // handleCancel=()=>{
-    //     this.setState({show:false})
-    //     this.setState({title:this.props.module.title})
-    // }
+  
     handlechange = (e) =>{
         console.log(this.state.selectedModule)
         let selectedModule = this.state.selectedModule;
@@ -35,55 +35,73 @@ class Module extends Component{
             this.props.onSave({...this.props.module},this.state.title)
             this.setState({show:false})
         }
+        handleTextChange= e => {
+            this.setState({explanation:e})
+          console.log(this.state.explanation);
+         };
         
     render(){
-
-    return(
-      
-        <li  className = 'module'>
-            <div> {this.props.module.title} 
-            </div>
-                <nav className="edit">
-                    <button className="glyphicon glyphicon-edit"
-                            onClick={this.handleDialoge}>
-                    </button>
-                    <Modal
+        const editorOptions = {
+            toolbar: [
+              [{ header: '1' }, { header: '2' }, { font: [] }],
+              [{ size: [] }],
+              ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+              [{ list: 'ordered' }, { list: 'bullet' }],
+              ['link', 'image', 'video'],
+              ['clean'],
+              ['code-block']
+            ]
+          };
+        return(
+        
+            <li  className = 'module'>
+                <div> {this.props.module.title} 
+                </div>
+                    <nav className="edit">
+                        <Button className="glyphicon glyphicon-edit"
+                                onClick={this.handleDialoge}>
+                        </Button>
+                        <Modal
                             show={this.state.show}
                             onHide={this.handleDialoge}
-                            aria-labelledby="contained-modal-title">
-                        <Modal.Header closeButton>
-                        <Modal.Title id="contained-modal-title">
-                                    Edit Module 
-                        </Modal.Title>
-                        <input 
-                                name='title'
-                                value= {this.props.module.title}
-                                placeholder = 'title'
-                                onChange = {this.handlechange}/>
-                        </Modal.Header>
-                       
-                        <Modal.Body>
-                        </Modal.Body>
-                        <Modal.Footer>
-                        {/* <Button onClick = {this.handleCancel}> Cancel </Button> */}
-                        <Button onClick = {this.handleSave}> Save </Button>
-                        <Button onClick={this.handleDialoge}>Close</Button>
-                        </Modal.Footer>
-                    </Modal>
-                    <button 
-                    className = 'glyphicon glyphicon-trash' 
-                    onClick={() => 
-                    {if (window.confirm(`Are you sure you want to delete?  `))
-                        this.props.onDelete( this.props.module._id);
-                    }}>
-                    
-                    </button>
-                
-                </nav>
-                
-        </li>
-        
-    )
-}
+                            aria-labelledby="contained-modal-title"
+                        >
+                            <Modal.Header closeButton>
+                                <Modal.Title id="contained-modal-title">
+                                Update module
+                                </Modal.Title>
+                                <h3>Title:</h3>                        
+                                <input 
+                                    name='title'
+                                    value= {this.props.module.title}
+                                    placeholder = 'title'
+                                    onChange = {this.handlechange}
+                                    />
+                            </Modal.Header>
+                                <h3>Contents for the explanation step:</h3>
+                            <Modal.Body>
+                                <ReactQuill 
+                                    modules={editorOptions}                                
+                                    placeholder="Contents"
+                                    onChange={this.handleTextChange}
+                                    // value={this.state.explanation}
+                                />   
+                            </Modal.Body>
+                            <Modal.Footer>
+                            <Button onClick = {this.handleSave}> Update modeule </Button>
+                            <Button onClick={this.handleDialoge}>Close</Button>
+                            </Modal.Footer>
+                        </Modal>
+                        <Button 
+                        className = 'glyphicon glyphicon-trash' 
+                        onClick={() => 
+                        {if (window.confirm(`Are you sure you want to delete?  `))
+                            this.props.onDelete( this.props.module._id);
+                        }}>                    
+                        </Button>                
+                    </nav>                
+            </li>        
+        )
+    }
 }
 export default Module
