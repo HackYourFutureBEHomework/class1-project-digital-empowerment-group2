defd8a326201e2fcaf194ce1616a8bc21f21eacf
Abdou Sanda Dourhamane <abdous2004@yahoo.fr>
2018-11-14 18:39:34 +0100

    update from yesterday

--- client/src/components/Modules.jsx
+++ client/src/components/Modules.jsx
@@ -1,31 +1,5 @@
 import React, { Component} from 'react';
 
-<<<<<<< HEAD
-import { getModules, createModule ,    deleteModule,
-updateModule } from '../api/modules';
-import EditModule from './EditModule';
-import Module from './Module';
-//import Trigger from './Trigger'
-import{ Button ,Modal} from 'react-bootstrap';
-
-class Modules extends Component {
-  //constructor(props) {
-
-    //super(props);
-    
-      //this.
-      state = {
-        
-      title:'',
-      modules: [],
-      selectedModule: null,
-      show:false
-                  };
-      
-   
-      
-//}
-=======
 import { getModules, createModule, deleteModule, updateModule } from '../api/modules';
 import EditModule from './EditModule'
 import Module from './Module'
@@ -34,27 +8,7 @@ import{ Button ,Modal} from 'react-bootstrap'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
->>>>>>> 0adda30083fdd324dec5aecb9e2e70022ed714a1
 
-
-HandleDialoge=() =>{
-  this.setState({ show: !this.state.show });
-}
-
-<<<<<<< HEAD
-  componentDidMount() {
-    getModules().then((modules) => {
-      this.setState({ modules: modules });
-    });
-  }
-  
-
-  handlingChange = e => {
-      this.setState({
-      title: e.target.value
-    });
-  };
-=======
 
 class Modules extends Component {
  state = {
@@ -68,12 +22,17 @@ class Modules extends Component {
       //Evaluation: '',
           };
 
+
+  
+    HandleDialoge=() =>{
+      this.setState({ show: !this.state.show });
+    }
+    
    componentDidMount() {
    getModules().then((modules) => {
       this.setState({  modules });
     });
   }
->>>>>>> 0adda30083fdd324dec5aecb9e2e70022ed714a1
 
   createModule = module => {
        createModule(this.state.title).then(newModule => {
@@ -84,48 +43,10 @@ class Modules extends Component {
       });
     });
   };
-<<<<<<< HEAD
- 
-
-
-  handleDelete =  id => { 
-    deleteModule(id);
-      this.setState({     
-        modules:this.state.modules.filter( mod => mod._id!== id )
-      });
-};
-
-// handleDelete = (event, id)=>{ 
-//   //event.stopPropagation();
-//   deleteModule(id)
-//   //.then(()=> {
-//     //let modules = this.state.modules;
-//     this.setState({
-//   modules:this.state.modules.filter( module=>module._id!== id )})
-//   //if (this.selectedModule === id){
-//     //this.setState ({selectedModule:null})
-//   }
-// //});
-// //}
-
-  handleSelect = (module) =>{
-    this.setState({ selectedModule: module})
-
-
-  }
-  
-
-  handeleSave = (module) => {
-    const {selectedModule}=this.state
-
-    updateModule(selectedModule).then((updatedModule) => {
-
-=======
 
   updateModule = (module) => {
     const {selectedModule}=this.state
     updateModule(selectedModule).then((updatedModule) => {
->>>>>>> 0adda30083fdd324dec5aecb9e2e70022ed714a1
       this.setState((previousState) => {
         const modules = [...previousState.modules];
         const index = modules.findIndex(mod => mod._id === selectedModule._id);
@@ -133,41 +54,6 @@ class Modules extends Component {
         return { modules ,selectedModule: null };
       })
     });
-<<<<<<< HEAD
-};
-
-  // handeleSave = (module) =>{
-  //   //const modules = this.state.modules;
-  //    updateModule(module)
-  //   .then(newModule => {
-  //     this.setState( (previousState) =>{
-  //       const modules = [...previousState.modules];
-  //       const index  = modules.findIndex(mod => mod._id === module._id);
-  //       modules[index] = newModule;
-  //       return{modules};
-        
-  //     });
-  //   });
-  //  }
-  // handeleSave = (module) =>{
-  //   //const modules = this.state.modules;
-  //   // if (this.state.addNewModule)
-  //   updateModule(module)
-  //   .then(newModule => {
-  //     this.setState((previousState)=>{
-  //       const modules = [...previousState.modules];
-  //       const index  = modules.findIndex(mod => mod._id === module._id);
-  //       modules[index] = newModule;
-  //       return{modules};
-  //     });
-  //   });
-  
-
-  // }
-
-  handleCancel = () =>{
-    this.setState({ selectedModule: null, edit: !this.state.edit})
-=======
   };
 
   deleteModule =  id => { 
@@ -176,7 +62,6 @@ class Modules extends Component {
         modules:this.state.modules.filter( mod => mod._id!== id )
       });
     };
->>>>>>> 0adda30083fdd324dec5aecb9e2e70022ed714a1
 
     showModuleFrom = () => {
       this.setState({ moduleFormShown: true });
@@ -255,22 +140,6 @@ class Modules extends Component {
     // so look  at it  
     const { modules} = this.state;
       return (
-<<<<<<< HEAD
-          <div>
-             <h2 > Using a web browser</h2>
-            {/* <h2>  Title of the active path</h2> */}
-            <fieldset className= ''>
-              <legend className='' >modules :</legend>
-              <div className = 'container2'>
-                <div className="modal-container">
-                        <Button 
-                            bsStyle="primary"
-                            bsSize="large"
-                            onClick={this.HandleDialoge}
-                            >
-                            Add module
-                          </Button>
-=======
         <div>
           <h2 > Using a web browser</h2>
             <fieldset className= ''>
@@ -278,59 +147,10 @@ class Modules extends Component {
                 <div className = 'container2'>
                   <div className="modal-container">
                       <Button type="button"  bsStyle="primary" className="button" onClick={this.HandleDialoge}>Add module</Button> 
->>>>>>> 0adda30083fdd324dec5aecb9e2e70022ed714a1
                           <Modal
                             show={this.state.show}
                             onHide={this.HandleDialoge}
                             aria-labelledby="contained-modal-title"
-<<<<<<< HEAD
-                          >
-                            <Modal.Header closeButton>
-                              <Modal.Title id="contained-modal-title">
-                                Add New Module
-                              </Modal.Title>
-                            </Modal.Header>
-                            <input type='text' placeholder='Enter The title' onChange={this.handlingChange}></input>
-                            <button onClick={this.addNewModule}>submit</button>
-                            <Modal.Body>
-                              you can add the 
-                            </Modal.Body>
-                            <Modal.Footer>
-                              <Button onClick={this.HandleDialoge}>Close</Button>
-                            </Modal.Footer>
-                          </Modal>
-                        </div>
-            </div>
-              <ul>
-                  {modules.map(module =>{ 
-                    return (
-                      <Module 
-                        key={module._id}
-                        module={module} 
-                        onSelect = {this.handleSelect} 
-                        selectedModule ={this.state.selectedModule}  
-                        onDelete = {this.handleDelete}
-                        //onEdit={this.handlechange}
-                      />)
-                    })}  
-              </ul>
-              <div className = 'editarea'> 
-                    <EditModule  
-                        selectedModule={this.state.selectedModule}
-                        onChange = {this.handlechange} 
-                        onSave = { this.handeleSave}
-                        onCancel = {this.handleCancel}
-                        onDelete = {this.handleDelete}
-                    />
-              </div>
-          </fieldset>
-          
-          </div>
-        )
-      
-    }
-  
-=======
                           >                         
                               <Modal.Header closeButton>
                                 <Modal.Title id="contained-modal-title">
@@ -396,7 +216,6 @@ class Modules extends Component {
         </div>
       )        
   }    
->>>>>>> 0adda30083fdd324dec5aecb9e2e70022ed714a1
 }
 
 export default Modules;
