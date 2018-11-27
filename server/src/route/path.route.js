6c2b9bb167cd3b1d784a14e628c30d296fbd2e11
radwan mohammed <mohammedfradwan@gmail.com>
2018-11-27 11:32:14 +0100

    Add Path.route to the server side and remove .env from gitIgnore

---
+++ server/src/route/path.route.js
@@ -0,0 +1,12 @@
+const modules = require('../controller/module.controller.js');
+const paths =require('../controller/path.controller.js')
+
+path.exports = (app) => {
+  app.get('/path', paths.findAll);
+  app.get('/path/:pathId', paths.findOne);
+  app.post('/path', paths.create);
+  app.post('/path/:pathId/module', modules.create);
+
+  app.delete('/path/:pathId', paths.destroy);
+  app.patch('/path/:pathId', paths.update);
+};
