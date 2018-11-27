5f636448db4d659adb8b7868cace7073aaa3c9c2
radwan mohammed <mohammedfradwan@gmail.com>
2018-11-27 11:33:21 +0100

    Add Model schema

---
+++ server/src/model/path.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+
+const PathSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    modules:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Module'
+    }]
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('Path', PathSchema);
