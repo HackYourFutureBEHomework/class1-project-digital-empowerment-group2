8c533a56024e8012383ee1506f596def31f568a1
Wameedh <wameedh.ali@hotmail.com>
2018-12-06 21:16:39 +0100

    no changes

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,59 +1,73 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Dialog,
+  FormGroup,
+  InputGroup,
+  Button
+} from '@blueprintjs/core';
+// import { cookies } from '../../constants';
 import * as api from '../../api/users';
 
- class  Login extends Component {
-    state = {
-        loginLoading: false,
-        email:'',
-        password:''
-    };
+import './login.css';
 
-    login = (e) => {
-        e.preventDefault();
-        this.setState({ loginLoading: true });
-    
-        const { email, password } = this.state;
-        const { completeLogin } = this.props;
-        api.login(email, password)
-          .then((res) => {
-            delete res.token;
-            completeLogin();
-          })
-          .catch(error => console.error(error));
-      };
-    //  login = (e) => {
-    //     const { email, password } = this.state;
-    //     e.preventDefault();
-    //     // console.log(email, password);
+class Login extends Component {
+  state = {
+    loginLoading: false,
+    email: '',
+    password: ''
+  };
 
-    //     const response = await fetch('http:// localhost:4000/api/login', {
-    //         method: 'POST',
-    //         headers,
-    //         body: JSON.stringify({ email, password })
-    //     });
-    //     const user = await response.json();
-    //     console.log(user);
-    //  }
+  login = (e) => {
+    e.preventDefault();
+    this.setState({ loginLoading: true });
 
-     setField = (e) => {
-        this.setState({ [e.currentTarget.name]: e.currentTarget.value });
-      }
+    const { email, password } = this.state;
+    const { completeLogin } = this.props;
+    api.login(email, password)
+      .then((res) => {
+        // cookies.set('auth', res.token);
+        delete res.token;
+        // cookies.set('user', res);
+        completeLogin();
+      })
+      .catch(error => console.error(error));
+  };
 
-      render() {
-         const { email, password } = this.state;
-          return (
-            <form onSubmit={ this.login}>
-                <label>
-                  Email:
-                  <input type="email" value={email} name="email" onChange={this.setField}/>
-                </label>
-                <label>
-                    Password:
-                    <input type="password" value={password} name="password" onChange={this.setField}/>
-                </label>
-                <input type="submit" value="log in"/>
-            </form>
-          )
-      }
- }
- export default Login;
+  setField = (e) => {
+    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+  }
+
+  render() {
+    const { loginLoading, email, password } = this.state;
+    const { cancelLogin } = this.props;
+
+    return (
+      <Dialog
+        isOpen
+        onClose={cancelLogin}
+        title="Log in"
+        className="dialog"
+      >
+        <div className="bp3-dialog-body">
+          <form onSubmit={this.login}>
+            <FormGroup label="Email" labelFor="login-email">
+              <InputGroup type="email" id="login-email" name="email" value={email} onChange={this.setField} required />
+            </FormGroup>
+            <FormGroup label="Password" labelFor="login-password">
+              <InputGroup type="password" id="login-password" name="password" value={password} onChange={this.setField} required />
+            </FormGroup>
+            <Button type="submit" intent="primary" loading={loginLoading}>Log in</Button>
+          </form>
+        </div>
+      </Dialog>
+    );
+  }
+}
+
+Login.propTypes = {
+  cancelLogin: PropTypes.func.isRequired,
+  completeLogin: PropTypes.func.isRequired
+};
+
+export default Login;
